ea3b5bc19b78ad9b45578103c005e43f09ec4270
Sascha Brink <sascha.brink@gmail.com>
2020-05-12 01:34:02 +0200

    Add bang to to_ts function

--- test/finance_ts/adapter/converstion_test.exs
+++ test/finance_ts/adapter/converstion_test.exs
@@ -5,10 +5,10 @@ defmodule FinanceTS.Adapter.ConversionTest do
 
   describe "#to_ts" do
     test "returns" do
-      assert Conversion.to_ts(315_532_800) == 315_532_800
-      assert Conversion.to_ts(~U[1980-01-01T00:00:00Z]) == 315_532_800
-      assert Conversion.to_ts("1980-01-01") == 315_532_800
-      assert Conversion.to_ts("19800101") == 315_532_800
+      assert Conversion.to_ts!(315_532_800) == 315_532_800
+      assert Conversion.to_ts!(~U[1980-01-01T00:00:00Z]) == 315_532_800
+      assert Conversion.to_ts!("1980-01-01") == 315_532_800
+      assert Conversion.to_ts!("19800101") == 315_532_800
     end
   end
 end
