d5dfe2a64d886c06d3445794eb8681d48ccec460
Sascha Brink <sascha.brink@gmail.com>
2020-05-11 23:05:53 +0200

    Change minimum required version to 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FinanceTS.MixProject do
     [
       app: :finance_ts,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
