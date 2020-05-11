b9c920e7fdfd5f26d7a92d7fb68700b638795604
Martin Schurig <martin@schurig.pw>
2020-05-11 23:18:37 +0200

    Remove wrong tests

--- test/finance_ts/adapters/bundesbank_test.exs
+++ test/finance_ts/adapters/bundesbank_test.exs
@@ -1,18 +1,10 @@
 defmodule FinanceTS.BundesbankAdapterTest do
   use FinanceTS.AdapterCase
-  @adapter FinanceTS.BundesbankAdapter
+  # @adapter FinanceTS.BundesbankAdapter
 
-  describe "#get_info" do
-    test "returns an info struct" do
-      assert_exchange_info @adapter.get_info()
-    end
-  end
-
-  describe "#coinlist" do
-    test "get the list" do
-      use_cassette "bundesbank#coinlist" do
-        assert @adapter.nothere() == []
-      end
-    end
+  describe "#get_gold_usd" do
+    # test "returns " do
+    #   assert_exchange_info @adapter.get_info()
+    # end
   end
 end
