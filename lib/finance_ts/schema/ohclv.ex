b4408776b12b06c815e8c888f1b04e8cf57cba35
Martin Schurig <martin@schurig.pw>
2020-05-11 21:43:24 +0200

    Add Tesla and remove Ecto

--- lib/finance_ts/schema/ohclv.ex
+++ lib/finance_ts/schema/ohclv.ex
@@ -1,20 +1,3 @@
 defmodule FinanceTS.Schema.OHCLV do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  schema "" do
-    field(:ts, :integer)
-    field(:o, :float)
-    field(:h, :float)
-    field(:c, :float)
-    field(:l, :float)
-    field(:v, :float)
-  end
-
-  @fields ~w(ts o h c l v)a
-  def changeset(meta, attrs, _) do
-    meta
-    |> cast(attrs, @fields)
-    |> validate_required(~w(ts)a)
-  end
+  defstruct [:ts, :o, :h, :c, :l, :v]
 end
--- lib/finance_ts/schema/time_series.ex
+++ lib/finance_ts/schema/time_series.ex
@@ -1,25 +1,6 @@
 defmodule FinanceTS.Schema.TimeSeries do
-  use Ecto.Schema
-  import Ecto.Changeset
-
-  alias FinanceTS.Schema.OHCLV
-
-  schema "" do
-    field(:symbol, :string)
-    field(:format, :string, default: "list")
-    field(:exchange, :string)
-    field(:currency, :string)
-    field(:first_ts, :integer)
-    field(:last_ts, :integer)
-    field(:latest_price, :float)
-
-    embeds_many(:data, OHCLV)
-  end
-
-  @fields ~w(symbol exchange currency)a
-  def changeset(meta, attrs, type) when type in [:create, :update] do
-    meta
-    |> cast(attrs, @fields)
-    |> validate_required(~w(symbol)a)
-  end
+  defstruct [
+    :symbol, :exchange, :currency, :first_ts, :last_ts, :latest_price,
+    format: "list", data: []
+  ]
 end
