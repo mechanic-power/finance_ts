7aef6bd486c23a219afe4920bb8c2ee2546f74ab
Sascha Brink <sascha.brink@gmail.com>
2020-05-12 03:08:44 +0200

    Add iso8601 date conversion

--- lib/finance_ts/adapter/conversion.ex
+++ lib/finance_ts/adapter/conversion.ex
@@ -31,4 +31,28 @@ defmodule FinanceTS.Adapter.Conversion do
     {:ok, datetime, 0} = DateTime.from_iso8601("#{y1}#{y2}-#{m}-#{d}T00:00:00Z")
     DateTime.to_unix(datetime)
   end
+
+  @doc """
+  Convert different types of date/datetime into a timestamp
+
+  iex> Conversion.to_iso8601_date!("1980-01-01")
+  "1980-01-01"
+
+  iex> Conversion.to_iso8601_date!("19800101")
+  "1980-01-01"
+
+  iex> Conversion.to_iso8601_date!(~D[1980-01-01])
+  "1980-01-01"
+
+  iex> Conversion.to_iso8601_date!(~U[1980-01-01T00:00:00Z])
+  "1980-01-01"
+  """
+  def to_iso8601_date!(str) when byte_size(str) == 10, do: str
+  def to_iso8601_date!(%Date{} = date), do: Date.to_string(date)
+  def to_iso8601_date!(%DateTime{} = datetime), do: Date.to_string(DateTime.to_date(datetime))
+
+  def to_iso8601_date!(str) when byte_size(str) == 8 do
+    [y1, y2, m, d] = String.split(str, ~r/(\d\d)/, include_captures: true, trim: true)
+    "#{y1}#{y2}-#{m}-#{d}"
+  end
 end
